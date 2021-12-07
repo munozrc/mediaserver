c7bcd0b81bfa6a107ab1597fc618e2dd8125be91
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-12-06 21:11:10 -0500

    feat(Server): Add init server with express

---
+++ server/src/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (request, response) => {
+  console.log(request.ip)
+  console.log(request.ips)
+  console.log(request.originalUrl)
+  response.send('<h1>Hello World!</h1>')
+})
+
+const PORT = process.env.PORT || 3001
+const server = app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+
+module.exports = { app, server }
