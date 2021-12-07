dbdbf0147d64a4f9e0729fa2250534a1e1b87f95
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-12-07 15:32:24 -0500

    feat(Client): Initial web client

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import styles from './styles/styles.module.css'
+
+const App = () => {
+  return (
+    <div className={styles.wrapper}>
+      <h1 className={styles.title}>Media Server</h1>
+    </div>
+  )
+}
+
+export default App
