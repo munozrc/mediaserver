87752708d03956ed5dfe733d168bdd25fd3af688
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-12-07 12:03:59 -0500

    feat(Server): Create router for movies

---
+++ server/src/controllers/movies.js
@@ -0,0 +1,38 @@
+const fs = require('fs')
+const router = require('express').Router()
+const moviesList = require('../data/movies.json')
+
+router.get('/', (_req, res) => {
+  res.send({ movies: moviesList })
+})
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params
+  const findMovie = moviesList.find(movie => movie.id === id)
+
+  if (typeof findMovie === 'undefined') return res.send({ message: 'movie not found' })
+
+  const { range } = req.headers
+  const stat = fs.statSync(findMovie.source)
+
+  if (range) {
+    const [partialStart, partialEnd] = range.replace(/bytes=/, '').split('-')
+    const start = parseInt(partialStart, 10)
+    const end = partialEnd ? parseInt(partialEnd, 10) : stat.size - 1
+    const file = fs.createReadStream(findMovie.source, { start: start, end: end })
+
+    res.writeHead(206, {
+      'Content-Range': 'bytes ' + start + '-' + end + '/' + stat.size,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': (end - start) + 1,
+      'Content-Type': 'video/mp4'
+    })
+
+    file.pipe(res)
+  } else {
+    res.writeHead(200, { 'Content-Length': stat.size, 'Content-Type': 'video/mp4' })
+    fs.createReadStream(findMovie.source).pipe(res)
+  }
+})
+
+module.exports = router
--- server/src/index.js
+++ server/src/index.js
@@ -1,32 +1,14 @@
 const express = require('express')
-const fs = require('fs')
-
 const app = express()
 
-app.get('/', (req, res) => {
-  const { range } = req.headers
-  const path = 'example.mp4'
-  const stat = fs.statSync(path)
+// Routers
+const moviesRouter = require('./controllers/movies')
 
-  if (range) {
-    const [partialStart, partialEnd] = range.replace(/bytes=/, '').split('-')
-    const start = parseInt(partialStart, 10)
-    const end = partialEnd ? parseInt(partialEnd, 10) : stat.size - 1
-    const file = fs.createReadStream(path, { start: start, end: end })
-
-    res.writeHead(206, {
-      'Content-Range': 'bytes ' + start + '-' + end + '/' + stat.size,
-      'Accept-Ranges': 'bytes',
-      'Content-Length': (end - start) + 1,
-      'Content-Type': 'video/mp4'
-    })
-
-    file.pipe(res)
-  } else {
-    res.writeHead(200, { 'Content-Length': stat.size, 'Content-Type': 'video/mp4' })
-    fs.createReadStream(path).pipe(res)
-  }
+app.get('/', (_req, res) => {
+  res.send({ message: 'Welcome to the API Media Server' })
 })
+
+app.use('/api/movies', moviesRouter)
 
 const PORT = process.env.PORT || 3001
 const server = app.listen(PORT, () => {
