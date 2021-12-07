7c46ef263e4a7771492bbd6e1ddf32f6c1ab2a01
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-12-06 22:32:24 -0500

    feat(Server): Add code for StreamVideo

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,31 @@
 const express = require('express')
+const fs = require('fs')
+
 const app = express()
 
-app.get('/', (request, response) => {
-  console.log(request.ip)
-  console.log(request.ips)
-  console.log(request.originalUrl)
-  response.send('<h1>Hello World!</h1>')
+app.get('/', (req, res) => {
+  const { range } = req.headers
+  const path = 'example.mp4'
+  const stat = fs.statSync(path)
+
+  if (range) {
+    const [partialStart, partialEnd] = range.replace(/bytes=/, '').split('-')
+    const start = parseInt(partialStart, 10)
+    const end = partialEnd ? parseInt(partialEnd, 10) : stat.size - 1
+    const file = fs.createReadStream(path, { start: start, end: end })
+
+    res.writeHead(206, {
+      'Content-Range': 'bytes ' + start + '-' + end + '/' + stat.size,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': (end - start) + 1,
+      'Content-Type': 'video/mp4'
+    })
+
+    file.pipe(res)
+  } else {
+    res.writeHead(200, { 'Content-Length': stat.size, 'Content-Type': 'video/mp4' })
+    fs.createReadStream(path).pipe(res)
+  }
 })
 
 const PORT = process.env.PORT || 3001
