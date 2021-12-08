a03f2e857390a99a766158bacdc1c497f5087943
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-12-07 22:32:15 -0500

    feat(Client): Create componente layout Header

---
+++ client/src/layouts/Header/index.jsx
@@ -0,0 +1,11 @@
+import styles from './styles.module.css'
+
+const Header = () => {
+  return (
+    <header className={styles.wrapper}>
+      <h1 className={styles.logo}>MediaServer</h1>
+    </header>
+  )
+}
+
+export default Header
