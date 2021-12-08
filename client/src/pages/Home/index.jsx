52facd42219e9ea937cba2c36d0ed0cfff9c193d
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-12-07 23:24:49 -0500

    feat(Client): use Card component render Items in Homepage

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,13 +1,17 @@
 import Container from '../../layouts/Container'
+import GridContainer from '../../layouts/GridContainer'
 import Header from '../../layouts/Header'
+import Card from './components/Card'
+
+const linkImage = 'https://m.media-amazon.com/images/M/MV5BNzU3NDg4NTAyNV5BMl5BanBnXkFtZTcwOTg2ODg1Mg@@._V1_FMjpg_UY740_.jpg'
 
 const Home = () => {
   return (
     <Container>
       <Header />
-      <section>
-        {[0, 1, 2, 3].map(ele => <a key={ele}> {ele} </a>)}
-      </section>
+      <GridContainer>
+        {[0, 1, 2, 3].map(ele => <Card key={ele} id={ele} image={linkImage} title='Harry Potter y El Misterio del Príncipe' />)}
+      </GridContainer>
     </Container>
   )
 }
