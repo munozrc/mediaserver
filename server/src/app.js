33d2cab90135b05da6644612c06b6ebe5b18b64c
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-12-08 00:55:28 -0500

    fix(Server): Add middlewares like Cors

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const moviesRouter = require('./controllers/movies')
 
 // Initializations
@@ -6,6 +7,11 @@ const app = express()
 
 // Settings
 app.set('port', process.env.PORT || 3001)
+
+// middlewares
+app.use(cors())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 
 // Routes
 app.get('/', (_req, res) => { res.send({ message: 'Welcome to the API Media Server' }) })
