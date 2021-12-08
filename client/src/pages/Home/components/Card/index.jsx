fc22761061acb6e5fafbe5abb040b9f85c7e25fb
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-12-07 23:53:55 -0500

    feat(Client): add new styles from Card

--- client/src/pages/Home/components/Card/index.jsx
+++ client/src/pages/Home/components/Card/index.jsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom'
+import { RiPlayFill } from 'react-icons/ri' // Remix Icons
 import styles from './styles.module.css'
 
 const Card = ({ title, image, id, mediaType = 'movies' }) => {
@@ -6,6 +7,7 @@ const Card = ({ title, image, id, mediaType = 'movies' }) => {
     <Link className={styles.wrapper} to={`/${mediaType}/${id}`}>
       <div className={styles.wrapperImage}>
         <img className={styles.image} src={image} alt={title} />
+        <RiPlayFill className={styles.play} color='#fafafa' size='5em' />
       </div>
       <h3 className={styles.title}>{title}</h3>
     </Link>
