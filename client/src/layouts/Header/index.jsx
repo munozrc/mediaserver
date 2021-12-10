30c46a253ad08170be6c73089e46caf056162be7
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-12-10 15:41:04 -0500

    feat(Client): Convert logo header as Link Component

--- client/src/layouts/Header/index.jsx
+++ client/src/layouts/Header/index.jsx
@@ -1,9 +1,10 @@
+import { Link } from 'react-router-dom'
 import styles from './styles.module.css'
 
 const Header = () => {
   return (
     <header className={styles.wrapper}>
-      <h1 className={styles.logo}>MediaServer</h1>
+      <Link to='/'><h1 className={styles.logo}>MediaServer</h1></Link>
     </header>
   )
 }
