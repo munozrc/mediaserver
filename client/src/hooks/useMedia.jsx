953850cda1a964d14152c61fe2fafa3452442586
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-12-10 15:30:21 -0500

    feat(Client): add hooks for use global state

--- client/src/hooks/useMedia.jsx
+++ client/src/hooks/useMedia.jsx
@@ -1,8 +1,9 @@
-import { useEffect, useState } from 'react'
+import { useContext, useEffect } from 'react'
+import MediaContext from '../context/mediaContext'
 import { getMovies } from '../services/movies'
 
 export default function useMedia () {
-  const [media, setMedia] = useState({ movies: [] })
+  const { media, setMedia } = useContext(MediaContext)
 
   useEffect(() => {
     getMovies().then((movies) => setMedia(prev => ({ ...prev, movies })))
---
+++ client/src/hooks/useSingleMedia.jsx
@@ -0,0 +1,14 @@
+import { useContext, useEffect, useState } from 'react'
+import MediaContext from '../context/mediaContext'
+
+export default function useSingleMedia ({ id }) {
+  const { media } = useContext(MediaContext)
+  const [activeMedia, setActiveMedia] = useState(null)
+
+  useEffect(() => {
+    const find = media.movies.find(ele => ele.id === id)
+    setActiveMedia(typeof find !== 'undefined' ? find : null)
+  }, [media])
+
+  return { activeMedia }
+}
