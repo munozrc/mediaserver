92f10bb2e939923e3e57d2933701e9b92ec201ae
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-12-10 15:32:58 -0500

    feat(Client): Add new routes and Component provider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,18 @@
-import Home from './pages/Home'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { MediaContextProvider } from './context/mediaContext'
+import Home from './pages/Home'
+import ContentDetail from './pages/Home/components/ContentDetail'
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home />} />
-      </Routes>
+      <MediaContextProvider>
+        <Routes>
+          <Route path='/' element={<Home />}>
+            <Route path='/detail/:id' element={<ContentDetail />} />
+          </Route>
+        </Routes>
+      </MediaContextProvider>
     </BrowserRouter>
   )
 }
