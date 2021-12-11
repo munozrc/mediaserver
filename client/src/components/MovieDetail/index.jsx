aaad7c1955ddfee8eeccabe5405dd39522ab7a68
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-12-10 19:53:47 -0500

    feat(client): Create component for show details media

---
+++ client/src/components/MovieDetail/index.jsx
@@ -0,0 +1,40 @@
+import VideoPlayer from '../VideoPlayer'
+import { useParams } from 'react-router-dom'
+import { getSourceVideo } from '../../services/movies'
+import useSingleMedia from '../../hooks/useSingleMedia'
+import Modal from '../../layouts/Modal'
+import { RiCloseFill } from 'react-icons/ri' // Remix Icons
+import styles from './styles.module.css'
+
+const MovieDetail = () => {
+  const { id } = useParams()
+  const { activeMedia } = useSingleMedia({ id })
+
+  if (activeMedia === null) return null
+  return (
+    <Modal>
+      <section className={styles.wrapper}>
+        <header className={styles.header}>
+          <div className={styles.wrapperImage}>
+            <img className={styles.image} src={activeMedia.gif} alt={`${id}-image`} />
+          </div>
+          <div className={styles.wrapperInfo}>
+            <h1 className={styles.title}>{activeMedia.title}</h1>
+          </div>
+          <button className={styles.btnClose}><RiCloseFill /></button>
+        </header>
+        <footer className={styles.footer}>
+          <div className={styles.videoPlayer}>
+            <VideoPlayer source={getSourceVideo({ id })} />
+          </div>
+          <p className={styles.synopsis}>
+            <strong className={styles.subtitle}>Sinopsis</strong>
+            {activeMedia.synopsis}
+          </p>
+        </footer>
+      </section>
+    </Modal>
+  )
+}
+
+export default MovieDetail
