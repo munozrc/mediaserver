f41eb88f85c663106851c8777c3168b2c21abc50
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-12-16 11:19:54 -0500

    feat(Client): Add config object to videoPlayer

--- client/src/components/VideoPlayer/index.jsx
+++ client/src/components/VideoPlayer/index.jsx
@@ -1,12 +1,25 @@
+import { useRef } from 'react'
 import ReactPlayer from 'react-player'
 import styles from './styles.module.css'
 
-const VideoPlayer = ({ source }) => {
+const VideoPlayer = ({ source, subtitles }) => {
+  const videoRef = useRef(null)
   return (
     <div className={styles.wrapper}>
       <ReactPlayer
+        ref={videoRef}
         className={styles.videoPlayer}
         url={source}
+        config={{
+          attributes: {
+            crossOrigin: 'true'
+          },
+          file: {
+            tracks: [
+              { kind: 'subtitles', src: subtitles, srcLang: 'es', default: true }
+            ]
+          }
+        }}
         width='100%'
         height='100%'
         controls
