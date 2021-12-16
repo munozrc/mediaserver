d8d86c9ea119c73691094699224938030b76cd76
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-12-16 11:18:51 -0500

    feat(Client): Add font for subtitles

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './styles/index.css'
 import App from './App'
+
+import './styles/index.css'
+import './assets/fonts/Lato-Black.ttf'
 
 ReactDOM.render(<App />, document.getElementById('root'))
