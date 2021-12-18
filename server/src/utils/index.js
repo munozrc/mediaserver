2ac53e747a7297f0a2585b3cbe13f7177b360e9a
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-12-18 09:38:29 -0500

    fix(Server): Add propertie subtitles if exists

--- server/src/utils/index.js
+++ server/src/utils/index.js
@@ -1,6 +1,9 @@
 const normalizeMovies = (movie) => {
-  const { id, poster, gif, title, synopsis } = movie
-  return { id, poster, gif, title, synopsis }
+  const { id, poster, gif, title, synopsis, subtitles } = movie
+  const obj = { id, poster, gif, title, synopsis }
+
+  if (subtitles !== '') return { ...obj, subtitles: true }
+  return { ...obj, subtitles: false }
 }
 
 module.exports = { normalizeMovies }
