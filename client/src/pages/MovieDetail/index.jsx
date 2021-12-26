298c765a517c32ba0f768aabddd7c35fd0628261
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-12-25 23:20:16 -0500

    feat(client): Use Button component

--- client/src/pages/MovieDetail/index.jsx
+++ client/src/pages/MovieDetail/index.jsx
@@ -2,9 +2,10 @@ import { useParams } from 'react-router-dom'
 import useSingleMedia from '../../hooks/useSingleMedia'
 import Container from '../../layouts/Container'
 import Header from '../../layouts/Header'
-import { RiPlayMiniFill } from 'react-icons/ri'
+import { RiPlayFill } from 'react-icons/ri'
 
 import styles from './styles.module.css'
+import Button from '../../components/Button'
 
 const MovieDetail = () => {
   const { id } = useParams()
@@ -21,7 +22,8 @@ const MovieDetail = () => {
               <h1 className={styles.titleMedia}>{activeMedia.title}</h1>
               <p className={styles.synopsis}>{activeMedia.synopsis}</p>
               <div className={styles.buttons}>
-                <button className={styles.btn}>Reproducir <RiPlayMiniFill /></button>
+                <Button><RiPlayFill size='28px' /></Button>
+                <Button variant='outline'>Trailer</Button>
               </div>
             </div>
           </header>
