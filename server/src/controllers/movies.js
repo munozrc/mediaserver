2feb559e280a80fe1946512663f32b5149bfe6c6
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-12-26 22:30:46 -0500

    fix(server): Change code videoStream

--- server/src/controllers/movies.js
+++ server/src/controllers/movies.js
@@ -2,10 +2,10 @@ const fs = require('fs')
 const router = require('express').Router()
 const { normalizeMovie } = require('../utils')
 const { getConfigFile } = require('../utils/files')
+const moviesList = getConfigFile('../data/movies.json')
 
 router.get('/subtitle', (req, res) => {
   const { id, source, lang } = req.query
-  const moviesList = getConfigFile('../data/movies.json')
 
   const movie = moviesList.find(movie => movie.id === id)
   if (typeof movie === 'undefined') return res.send('')
@@ -18,41 +18,47 @@ router.get('/subtitle', (req, res) => {
 
 router.get('/:id', (req, res) => {
   const { id } = req.params
-  const moviesList = getConfigFile('../data/movies.json')
-  const findMovie = moviesList.find(movie => movie.id === id)
 
+  const findMovie = moviesList.find(movie => movie.id === id)
   if (typeof findMovie === 'undefined') return res.send({ message: 'movie not found' })
 
-  const { range } = req.headers
-  const pathSourceMovie = findMovie.sources[0].path
-  const stat = fs.statSync(pathSourceMovie)
+  // Ensure there is a range given for the video
+  const range = req.headers.range
+  if (!range) return res.status(400).send('Requires Range header')
 
-  if (range) {
-    const [partialStart, partialEnd] = range.replace(/bytes=/, '').split('-')
-    const start = parseInt(partialStart, 10)
-    const end = partialEnd ? parseInt(partialEnd, 10) : stat.size - 1
-    const file = fs.createReadStream(pathSourceMovie, { start: start, end: end })
+  // get video stats (about 61MB)
+  const videoPath = findMovie.sources[0].path
+  const videoSize = fs.statSync(findMovie.sources[0].path).size
 
-    res.writeHead(206, {
-      'Content-Range': 'bytes ' + start + '-' + end + '/' + stat.size,
-      'Accept-Ranges': 'bytes',
-      'Content-Length': (end - start) + 1,
-      'Content-Type': 'video/mp4'
-    })
+  // Parse Range
+  // Example: "bytes=32324-"
+  const CHUNK_SIZE = 10 ** 6 // 1MB
+  const start = Number(range.replace(/\D/g, ''))
+  const end = Math.min(start + CHUNK_SIZE, videoSize - 1)
 
-    file.pipe(res)
-  } else {
-    res.writeHead(200, { 'Content-Length': stat.size, 'Content-Type': 'video/mp4' })
-    fs.createReadStream(pathSourceMovie).pipe(res)
+  // Create headers
+  const contentLength = end - start + 1
+  const headers = {
+    'Content-Range': `bytes ${start}-${end}/${videoSize}`,
+    'Accept-Ranges': 'bytes',
+    'Content-Length': contentLength,
+    'Content-Type': 'video/mp4'
   }
+
+  // HTTP Status 206 for Partial Content
+  res.writeHead(206, headers)
+
+  // create video read stream for this particular chunk
+  const videoStream = fs.createReadStream(videoPath, { start, end })
+
+  // Stream the video chunk to the client
+  videoStream.pipe(res)
 })
 
 router.get('/', (req, res) => {
   const { protocol, hostname } = req
-  const list = getConfigFile('../data/movies.json')
-
   const originURL = `${protocol}://${hostname}:${process.env.PORT || 3001}/api`
-  const movies = list.map((movie) => normalizeMovie({ movie, originURL }))
+  const movies = moviesList.map((movie) => normalizeMovie({ movie, originURL }))
   res.send({ movies })
 })
 
