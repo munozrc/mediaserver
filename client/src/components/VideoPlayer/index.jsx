c881399c27f58bd2bbe6923de40bed49a2c26e0b
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-12-28 12:09:41 -0500

    feat(client): use slider and add states for VideoPlayer

--- client/src/components/VideoPlayer/index.jsx
+++ client/src/components/VideoPlayer/index.jsx
@@ -1,23 +1,44 @@
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import ReactPlayer from 'react-player'
+import SliderSeek from './components/SliderSeek'
 import styles from './styles.module.css'
 
 const VideoPlayer = ({ source, subtitles = {} }) => {
   const videoRef = useRef(null)
+  const [values, setValues] = useState({ loaded: 0, played: 0, duration: 0, volume: 0.5 })
+  const [isPlaying, setPlaying] = useState(false)
+  const [counter, setCounter] = useState(0)
+
+  const handleProgress = ({ loadedSeconds, playedSeconds }) => {
+    setValues(prev => ({ ...prev, loaded: loadedSeconds, played: playedSeconds }))
+    setCounter(prev => prev + 1)
+  }
+
+  const handleDurationVideo = (duration) => { setValues(prev => ({ ...prev, duration })) }
+
+  const handleChangeSeek = (value) => { videoRef.current?.seekTo(value, 'seconds') }
+
   return (
     <div className={styles.wrapper}>
       <ReactPlayer
         ref={videoRef}
+        width='100%'
+        height='100%'
         className={styles.videoPlayer}
         url={source}
+        playing={isPlaying}
+        volume={values.volume}
+        onProgress={handleProgress}
+        onDuration={handleDurationVideo}
+        onPause={() => setPlaying(false)}
         config={{
           attributes: { crossOrigin: 'true' },
           file: { tracks: [...subtitles] }
         }}
-        width='100%'
-        height='100%'
-        controls
       />
+      <section>
+        <SliderSeek currentTime={values.played} duration={values.duration} onChangeSeek={handleChangeSeek} />
+      </section>
     </div>
   )
 }
