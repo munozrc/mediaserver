c3df2d4782d70a24b4867b2c1bac455a2e722b12
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-12-28 17:26:04 -0500

    feat(client): use MediaCard

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,20 +1,18 @@
-import { Outlet } from 'react-router-dom'
 import useMedia from '../../hooks/useMedia'
 import Container from '../../layouts/Container'
 import GridContainer from '../../layouts/GridContainer'
 import Header from '../../layouts/Header'
-import Card from './components/Card'
+import MediaCard from '../../components/MediaCard'
 
 const Home = () => {
   const { media } = useMedia()
   return (
     <Container>
       <Header />
-      <Outlet />
       <GridContainer>
         {
             media.movies.map(movie => (
-              <Card
+              <MediaCard
                 key={movie.id}
                 id={movie.id}
                 image={movie.poster}
