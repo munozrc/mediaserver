d149747f8c6d9435de868d1fd9445dfd5172e16a
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-12-28 17:25:31 -0500

    feat(client): Create MediaCard Component

---
+++ client/src/components/MediaCard/index.jsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom'
+import { RiPlayFill } from 'react-icons/ri' // Remix Icons
+import styles from './styles.module.css'
+
+const MediaCard = ({ type = 'movie', title, image, id }) => {
+  return (
+    <Link className={styles.wrapper} to={`/${type}/${id}`}>
+      <div className={styles.wrapperImage}>
+        <img className={styles.image} src={image} alt={title} />
+        <RiPlayFill className={styles.play} color='#fafafa' size='5em' />
+      </div>
+    </Link>
+  )
+}
+
+export default MediaCard
