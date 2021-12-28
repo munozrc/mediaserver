def4ebfdc6a338eea11b5073c572a28191733e78
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-12-28 12:09:11 -0500

    feat(client): create SliderSeek component for videoPlayer

---
+++ client/src/components/VideoPlayer/components/SliderSeek/index.jsx
@@ -0,0 +1,25 @@
+import React, { useRef } from 'react'
+import { getProgressInPixels } from '../../utils'
+import styles from './styles.module.css'
+
+const SliderSeek = ({ currentTime, duration, onChangeSeek }) => {
+  const seekRef = useRef(null)
+
+  const handleOnMouse = (event) => {
+    if (seekRef.current === null) return
+    const sr = seekRef.current
+    const normalizePageX = event.pageX - sr.getBoundingClientRect().left
+    const seek = ((normalizePageX - sr.offsetLeft) / sr.offsetWidth) * duration
+    onChangeSeek(seek)
+  }
+
+  return (
+    <div className={styles.wrapper}>
+      <div ref={seekRef} className={styles.seek} onClick={handleOnMouse}>
+        <div className={styles.track} style={{ width: getProgressInPixels({ currentTime, duration }) }} />
+      </div>
+    </div>
+  )
+}
+
+export default SliderSeek
