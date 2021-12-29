e7d3cba10ca2c5e60515220bb33ad884e2b60f5a
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-12-28 21:42:15 -0500

    fix(client): export isStartFullScreen

--- client/src/components/VideoPlayer/hooks/useFullScreen.jsx
+++ client/src/components/VideoPlayer/hooks/useFullScreen.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 import { FullScreen, useFullScreenHandle } from 'react-full-screen'
 
 const START_FULL_SCREEN = 'startFullScreenValue'
@@ -6,16 +6,20 @@ const DEFAULT_VALUE = true
 
 export default function useFullScreen () {
   const screen = useFullScreenHandle()
+  const startFullScreen = useRef(JSON.parse(window.localStorage.getItem(START_FULL_SCREEN) || DEFAULT_VALUE))
 
   const toggleFullScreen = () => screen.active ? screen.exit() : screen.enter()
 
   useEffect(() => {
-    const startFullScreen = JSON.parse(window.localStorage.getItem(START_FULL_SCREEN) || DEFAULT_VALUE)
-    window.localStorage.setItem(START_FULL_SCREEN, JSON.stringify(startFullScreen))
-    startFullScreen && screen.enter()
+    window.localStorage.setItem(START_FULL_SCREEN, JSON.stringify(startFullScreen.current))
+    startFullScreen.current && screen.enter()
   }, [])
 
-  return { screen, toggleFullScreen }
+  return {
+    screen,
+    isStartFullScreen: startFullScreen.current,
+    toggleFullScreen
+  }
 }
 
 export { FullScreen as WrapperFullScreen }
