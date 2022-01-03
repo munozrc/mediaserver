97471f9d751fc495cf71f59434a72c0a5b488b3e
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-01-03 14:57:29 -0500

    feat(client): Create SerieDetail page

---
+++ client/src/pages/SerieDetail/index.jsx
@@ -0,0 +1,54 @@
+import { useState } from 'react'
+import { useParams } from 'react-router-dom'
+import useSerie from '../../hooks/useSerie'
+import Container from '../../layouts/Container'
+import Header from '../../layouts/Header'
+import Separator from '../../layouts/Separator'
+import HeroMedia from '../../layouts/HeroMedia'
+import VideoPlayer from '../../components/VideoPlayer'
+
+const SerieDetail = () => {
+  const { id } = useParams()
+  const { serie, source, subtitles } = useSerie({ id })
+  const [showVideoPlayer, setShowVideoPlayer] = useState(false)
+
+  const buttonsHeroMedia = [
+    {
+      onClick: () => setShowVideoPlayer(true),
+      children: HeroMedia.IconPlay
+    },
+    {
+      variant: 'outline',
+      children: 'Trailer'
+    }
+  ]
+
+  console.log({ serie })
+
+  return (
+    <Container>
+      <Header />
+      {serie && (
+        <HeroMedia
+          id={id}
+          image={serie.images[0]}
+          title={serie.title}
+          synopsis={serie.synopsis}
+          buttons={buttonsHeroMedia}
+        />
+      )}
+      {
+        (serie && showVideoPlayer) && (
+          <VideoPlayer
+            source={{ title: serie.title, url: source }}
+            subtitles={subtitles}
+            handleClose={() => setShowVideoPlayer(false)}
+          />
+        )
+      }
+      <Separator />
+    </Container>
+  )
+}
+
+export default SerieDetail
