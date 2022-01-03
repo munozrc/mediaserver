451ffabc05ab9655b18c1e97fa4454ae9ce08b1f
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-01-03 11:57:07 -0500

    feat(Client): Use new pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { MediaContextProvider } from './context/mediaContext'
 import Home from './pages/Home'
+import Movies from './pages/Movies'
+import Series from './pages/Series'
 import MovieDetail from './pages/MovieDetail'
 
 const App = () => {
@@ -9,6 +11,8 @@ const App = () => {
       <MediaContextProvider>
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/movies' element={<Movies />} />
+          <Route path='/series' element={<Series />} />
           <Route path='/movie/:id' element={<MovieDetail />} />
         </Routes>
       </MediaContextProvider>
