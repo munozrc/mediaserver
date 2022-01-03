b98c281b1b027261b62b1dd2991f0bcda625c17a
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-01-03 15:22:00 -0500

    feat(Client): Add state for activeEpisode

--- client/src/hooks/useSerie.jsx
+++ client/src/hooks/useSerie.jsx
@@ -1,18 +1,23 @@
 import { useContext, useEffect, useState } from 'react'
 import MediaContext from '../context/mediaContext'
 
-export default function useSerie ({ id }) {
+export default function useSerie ({ id, initEpisode = 0 }) {
   const { media } = useContext(MediaContext)
   const [activeSerie, setActiveSerie] = useState(null)
+  const [activeEpsiode, setActiveEpisode] = useState(null)
 
   useEffect(() => {
     const serie = media.series.find(serie => serie.id === id)
-    if (typeof serie !== 'undefined') setActiveSerie(serie)
+    if (typeof serie === 'undefined') return
+
+    const episode = serie.episodes.map(episode => episode.id === initEpisode)
+    if (typeof episode !== 'undefined') setActiveEpisode(episode)
+
+    setActiveSerie(serie)
   }, [media, id])
 
   return {
     serie: activeSerie,
-    source: `/api/series/${id}`,
-    subtitles: {}
+    source: `/api/series/media?serie=${id}&epsiode=${activeEpsiode?.id}`
   }
 }
