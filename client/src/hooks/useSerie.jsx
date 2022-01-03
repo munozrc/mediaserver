8d76e82efdab427b0526fbbcb378c63937c76919
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-01-03 17:23:08 -0500

    fix(Client): fix source get for series

--- client/src/hooks/useSerie.jsx
+++ client/src/hooks/useSerie.jsx
@@ -1,16 +1,17 @@
 import { useContext, useEffect, useState } from 'react'
 import MediaContext from '../context/mediaContext'
 
-export default function useSerie ({ id, initEpisode = 0 }) {
+export default function useSerie ({ id, initEpisode = 1 }) {
   const { media } = useContext(MediaContext)
   const [activeSerie, setActiveSerie] = useState(null)
   const [activeEpsiode, setActiveEpisode] = useState(null)
+  const [activeSource] = useState(0)
 
   useEffect(() => {
     const serie = media.series.find(serie => serie.id === id)
     if (typeof serie === 'undefined') return
 
-    const episode = serie.episodes.map(episode => episode.id === initEpisode)
+    const episode = serie.episodes.find(episode => episode.id === initEpisode)
     if (typeof episode !== 'undefined') setActiveEpisode(episode)
 
     setActiveSerie(serie)
@@ -18,6 +19,7 @@ export default function useSerie ({ id, initEpisode = 0 }) {
 
   return {
     serie: activeSerie,
-    source: `/api/series/media?serie=${id}&epsiode=${activeEpsiode?.id}`
+    episode: activeEpsiode,
+    source: activeEpsiode?.sources[activeSource]
   }
 }
