2861281ec540f9118fecb2d0c322322cc8b9475e
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-01-03 14:55:49 -0500

    feat(Client): Create HeroMedia component

---
+++ client/src/layouts/HeroMedia/index.jsx
@@ -0,0 +1,32 @@
+import { RiPlayFill, RiCloseFill } from 'react-icons/ri'
+import Button from '../../components/Button'
+import LinkButton from '../../components/LinkButton'
+import styles from './styles.module.css'
+
+const HeroMedia = ({ image = '', title = '', synopsis = '', id = '', buttons = [] }) => {
+  return (
+    <section className={styles.section}>
+      <header className={styles.hero}>
+        <img className={styles.heroImage} src={image} alt={`${id}-hero-image`} />
+        <div className={styles.heroInfo}>
+          <header className={styles.heroHeader}>
+            <LinkButton to='/'><RiCloseFill size='1.8em' /></LinkButton>
+            <h1 className={styles.titleMedia}>{title}</h1>
+          </header>
+          <p className={styles.synopsis}>{synopsis}</p>
+          <div className={styles.buttons}>
+            {
+              buttons.map((button, index) => (
+                <Button key={index} {...button}>{button.children}</Button>
+              ))
+            }
+          </div>
+        </div>
+      </header>
+    </section>
+  )
+}
+
+HeroMedia.IconPlay = <RiPlayFill size='28px' />
+
+export default HeroMedia
