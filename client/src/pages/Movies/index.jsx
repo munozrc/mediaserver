3d8b02580d9364ab946763a0f8cc27c360ab9ed6
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-01-03 11:56:36 -0500

    feat(Client): Create MoviesPage

---
+++ client/src/pages/Movies/index.jsx
@@ -0,0 +1,28 @@
+import useMedia from '../../hooks/useMedia'
+import Container from '../../layouts/Container'
+import GridContainer from '../../layouts/GridContainer'
+import Header from '../../layouts/Header'
+import MediaCard from '../../components/MediaCard'
+
+const Movies = () => {
+  const { media } = useMedia()
+  return (
+    <Container>
+      <Header />
+      <GridContainer>
+        {
+          media.movies.map(movie => (
+            <MediaCard
+              key={movie.id}
+              id={movie.id}
+              image={movie.poster}
+              title={movie.title}
+            />
+          ))
+        }
+      </GridContainer>
+    </Container>
+  )
+}
+
+export default Movies
