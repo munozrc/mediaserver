452a1f771d41f73ac02768f9e9d9b9b656c1fd6c
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-01-04 18:46:58 -0500

    fix(Client): validation is true for one second

--- client/src/components/VideoPlayer/components/ButtonSkip/index.jsx
+++ client/src/components/VideoPlayer/components/ButtonSkip/index.jsx
@@ -4,12 +4,12 @@ import styles from './styles.module.css'
 
 const SECONDS_FOR_SHOW = 300
 
-const ButtonSkip = ({ currentTime = 0, duration = 0, handleClick }) => {
+const ButtonSkip = ({ currentTime, duration, handleClick }) => {
   const time = getTimeRemaining({ currentTime, duration })
   return (
     <button
       className={styles.buttonSkip}
-      style={{ visibility: (time > SECONDS_FOR_SHOW) ? 'hidden' : '' }}
+      style={{ visibility: (duration === 0 || time > SECONDS_FOR_SHOW) ? 'hidden' : 'visible' }}
       onClick={handleClick}
     >
       <RiSkipForwardFill size='1.4em' />Siguiente Episodio
