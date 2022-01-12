4945e0a9f612ccb227878f3573c08dfcb2ef5fa1
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-01-11 22:05:40 -0500

    fix(Client): Add test with other function fullscreen videoplayer

--- client/src/components/VideoPlayer/hooks/useFullScreen.jsx
+++ client/src/components/VideoPlayer/hooks/useFullScreen.jsx
@@ -5,18 +5,32 @@ const START_FULL_SCREEN = 'startFullScreenValue'
 const DEFAULT_VALUE = true
 
 export default function useFullScreen () {
-  const screen = useFullScreenHandle()
+  const screenFullscreen = useFullScreenHandle()
   const startFullScreen = useRef(JSON.parse(window.localStorage.getItem(START_FULL_SCREEN) || DEFAULT_VALUE))
 
-  const toggleFullScreen = () => screen.active ? screen.exit() : screen.enter()
+  const toggleFullScreen = () => screenFullscreen.active ? screenFullscreen.exit() : screenFullscreen.enter()
 
   useEffect(() => {
+    const { innerWidth, innerHeight, screen } = window
+    const { orientation } = screen
+
     window.localStorage.setItem(START_FULL_SCREEN, JSON.stringify(startFullScreen.current))
-    startFullScreen.current && screen.enter()
+
+    startFullScreen.current && screenFullscreen.enter()
+
+    if (innerWidth <= 500 && innerHeight <= 767 && orientation.type === 'portrait-primary') {
+      screen.orientation.lock('landscape-primary')
+        .then(function () {
+          console.log('change')
+        })
+        .catch(function (error) {
+          console.error(error)
+        })
+    }
   }, [])
 
   return {
-    screen,
+    screen: screenFullscreen,
     isStartFullScreen: startFullScreen.current,
     toggleFullScreen
   }
