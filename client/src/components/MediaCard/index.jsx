7ed732dd67160bf2d88d00fd0420a7a909674edd
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-01-15 21:37:01 -0500

    fix(Client): Add default title styles css

--- client/src/components/MediaCard/index.jsx
+++ client/src/components/MediaCard/index.jsx
@@ -1,12 +1,14 @@
-import { Link } from 'react-router-dom'
 import { RiPlayFill } from 'react-icons/ri' // Remix Icons
+import { Link } from 'react-router-dom'
 import styles from './styles.module.css'
 
 const MediaCard = ({ type = 'movies', title, image, id }) => {
   return (
     <Link className={styles.wrapper} to={`/${type}/${id}`}>
       <div className={styles.wrapperImage}>
-        <img className={styles.image} src={image} alt={title} />
+        <object className={styles.image} data={image}>
+          <span className={styles.title}>{title}</span>
+        </object>
         <RiPlayFill className={styles.play} color='#fafafa' size='5em' />
       </div>
     </Link>
